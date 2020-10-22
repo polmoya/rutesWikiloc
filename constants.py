d2ab012a5baec72878fa68641840d98f510bc73a
xmartinbr <67483619+xmartinbr@users.noreply.github.com>
2020-10-22 18:42:30 +0200

    Add files via upload
    Fitxers que utilitzo per fer el WebScrapping del site wikiloc

---
+++ constants.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 18:29:19 2020
+
+@author: XMB1483
+"""
+
+LOG_FILE = 'MainWebScrapping.log';
+MAIN_URL = 'http://es.wikiloc.com';
+ROBOTS_FILE = 'robots.txt';
+RUTAS_PAGE = '/rutas';
+QTY_TAGSLEVEL1 = 2;
+QTY_TAGSLEVEL2 = 2;
+QTY_TAGSLEVEL3 = 2;
+QTY_TAGSLEVEL4 = 0;
+H1_ACTIVIDADES = 'ACTIVIDADES';
+LEVEL1 = 'Level1';
